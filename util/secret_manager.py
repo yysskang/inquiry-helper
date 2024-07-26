fec544731a4881f6a206278a229126d438b3f68e
yskang <176553264+yysskang@users.noreply.github.com>
2024-07-27 02:33:36 +0900

    merge

---
+++ util/secret_manager.py
@@ -0,0 +1,25 @@
+import os
+
+import boto3
+import json
+
+
+class SecretManager:
+    def __init__(self: "SecretManager"):
+        self.secret_name = "nursing/prod"
+        self.secrets_manager_client = boto3.client(
+            service_name="secretsmanager",
+            region_name="ap-northeast-2",
+            aws_access_key_id=os.getenv("ACCESS_KEY", ""),
+            aws_secret_access_key=os.getenv("SECRET_KEY", "")
+        )
+
+    def secret_data(self, key):
+        response = self.secrets_manager_client.get_secret_value(SecretId=self.secret_name)
+        secret_data = json.loads(response['SecretString'])
+        aws_data = secret_data.get(key, None)
+
+        if aws_data:
+            return aws_data
+        else:
+            return ""
