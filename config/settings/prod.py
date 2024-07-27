b215a9d4afd1b9ac680578dee195f0cef20c6c47
yskang <176553264+yysskang@users.noreply.github.com>
2024-07-27 16:16:36 +0900

    chore: PROD DB 연동

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,4 @@
 from .base import *
-
 ALLOWED_HOSTS = ["*"]
 DEBUG = False
 
@@ -61,3 +60,20 @@ DEBUG = False
 #         },
 #     }
 # }
+
+import pymysql
+pymysql.install_as_MySQLdb()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': secrets.secret_data("DB_HOST"),
+        'PORT': 3306,
+        'NAME': secrets.secret_data("DB_NAME"),
+        'USER': secrets.secret_data("DB_USER"),
+        'PASSWORD': secrets.secret_data("DB_PASSWORD"),
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',
+        },
+    }
+}
+
