21411ce69c844aedeec70ccfb86247d4f6f5d1f4
yskang <176553264+yysskang@users.noreply.github.com>
2024-07-30 00:29:15 +0900

    merge

--- apps/user/site/views.py
+++ apps/user/site/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
@@ -8,7 +9,17 @@ class Index(TemplateView):
 class Login(TemplateView):
     template_name = "user/login.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.session.get("contract_id"):
+            return redirect('contract:contract-info')
+        return super().dispatch(request, *args, **kwargs)
+
 
 class Signup(TemplateView):
     template_name = "user/signup.html"
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.session.get("contract_id"):
+            return redirect('contract:contract-info')
+        return super().dispatch(request, *args, **kwargs)
+
 
